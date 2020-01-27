539fce59c1ae15c4e24e987c7871944c9ffa8d41
somoye123 <somoye.ayotunde@gmail.com>
2020-01-26 16:53:07 -0800

    add delete functionality

--- main.js
+++ main.js
@@ -21,15 +21,31 @@ document.getElementById('submit').addEventListener('click', onFormSubmit)
 
 async function onFormSubmit () {
     const body = {
-        Firstname: document.getElementsByName("firstName")[0].value,
-        Lastname: document.getElementsByName("lastName")[0].value,
-        Arrival_time: document.getElementsByName("arrivalTime")[0].value,
-        Exit_time: document.getElementsByName("exitTime")[0].value,
+        firstname: document.getElementsByName("firstName")[0].value,
+        lastname: document.getElementsByName("lastName")[0].value,
+        arrival_time: document.getElementsByName("arrivalTime")[0].value,
+        exit_time: document.getElementsByName("exitTime")[0].value,
         date_of_interview: document.getElementsByName("dateOfInterview")[0].value
     }
     try {
         console.log(body);
       const response = await axios.post("https://cors-anywhere.herokuapp.com/https://empl-dev.site/api/interview/addWork",body);
+      console.log(response);
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+document.getElementById('deletebutton').addEventListener('click', onDeleteFormSubmit)
+
+async function onDeleteFormSubmit () {
+    const body = {
+        id: document.getElementsByName("deleteInput")[0].value
+    }
+    try {
+        console.log(body);
+      const response = await axios.post("https://cors-anywhere.herokuapp.com/https://empl-dev.site/api/interview/deleteWork",body);
       console.log(response);
     } catch (error) {
         console.log(error)
